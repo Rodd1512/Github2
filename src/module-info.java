a004b56aa6fbe41d25c7f13f7b2d469e9c2b0827
RodrigoBejar-AlumnoC <RodrigoBejar-AlumnoC@CEACFP20692>
2023-05-12 11:33:57 +0200

    He puesto un hola mundo

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author RodrigoBejar-AlumnoC
+ *
+ */
+module PSencillo {
+}
---
+++ src/sencillo/Sencillo.java
@@ -0,0 +1,21 @@
+package sencillo;
+
+import java.util.Scanner;
+
+public class Sencillo {
+	
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce un número: ");
+        int num = scanner.nextInt();
+        int suma = num + 5;
+        int division = num / 2;
+        int cuadrado = num * num;
+        System.out.println("El número ingresado es: " + num);
+        System.out.println("El resultado de sumar 5 al número es: " + suma);
+        System.out.println("El resultado de dividir el número entre 2 es: " + division);
+        System.out.println("El resultado de elevar el número al cuadrado es: " + cuadrado);
+        System.out.println("HolaMundo");
+        scanner.close();
+    }
+}
